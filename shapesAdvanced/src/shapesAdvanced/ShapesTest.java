99c88832be623df020bc49f52130474773350927
fabien <fabien.kunis@gmail.com>
2015-05-20 03:51:03 +0300

    Add shapesAdvanced project

---
+++ shapesAdvanced/src/shapesAdvanced/ShapesTest.java
@@ -0,0 +1,60 @@
+package shapesAdvanced;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class ShapesTest {
+
+	private static final Random RANDOM = new Random();
+
+	@Test
+	public void equalRectanglesMustBeEqual() {
+		for (int i = 0; i < 100; i++) {
+			Rectangle r1 = arbitraryRectangle(), r2 = new Rectangle(
+					r1.getWidth(), r1.getHeight());
+			assertEquals(r2, r1);
+		}
+	}
+
+	@Test
+	public void equalSquaresMustBeEqual() {
+		for (int i = 0; i < 100; i++) {
+			Square s1 = arbitrarySquare(), s2 = new Square(s1.getWidth());
+			assertEquals(s2, s1);
+		}
+	}
+
+	@Test
+	public void setRectangleWidthShouldChangeOnlyWidth() {
+		for (int i = 0; i < 100; i++) {
+			Rectangle r = arbitraryRectangle();
+			int width = RANDOM.nextInt(50), height = r.getHeight();
+			r.setWidth(width);
+			assertEquals(width, r.getWidth());
+			assertEquals(height, r.getHeight());
+		}
+	}
+
+	@Test
+	public void squaresMustBeSquare() {
+		for (int i = 0; i < 100; i++) {
+			Square s = arbitrarySquare();
+			int side = RANDOM.nextInt(50);
+			s.setWidth(side);
+			assertEquals(side, s.getWidth());
+			assertEquals(side, s.getHeight());
+		}
+	}
+
+	private Rectangle arbitraryRectangle() {
+		return new Rectangle(RANDOM.nextInt(50), RANDOM.nextInt(50));
+	}
+
+	private Square arbitrarySquare() {
+		return new Square(RANDOM.nextInt(50));
+	}
+
+}
---
+++ shapesAdvanced/src/shapesAdvanced/Square.java
@@ -0,0 +1,9 @@
+package shapesAdvanced;
+
+public class Square extends Rectangle {
+
+	public Square(int side) {
+		super(side, side);
+	}
+
+}
