0aa94f73fc7326f919a6516f8aa0fe6502480b24
fabien <fabien.kunis@gmail.com>
2015-05-28 04:27:57 +0300

    Fixed race condition

--- concurrency/src/concurrency/Fibonacci.java
+++ concurrency/src/concurrency/Fibonacci.java
@@ -1,6 +1,8 @@
 package concurrency;
 
 import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,24 +16,59 @@ public class Fibonacci {
 		return n < 2 ? BigInteger.ONE : fib(n - 1).add(fib(n - 2));
 	}
 
-	public static void main(String[] args) throws InterruptedException {
+	public static class FibonacciTask implements Runnable {
+
+		private final Logger logger;
+
+		public FibonacciTask(Logger logger) {
+			this.logger = logger;
+		}
+
+		@Override
+		public void run() {
+			int n = RANDOM.nextInt(30);
+			logger.log(String.format("fib(%d) = %s", n, fib(n)));
+		}
+	}
+
+	public static class Logger implements Runnable {
+
+		private final Queue<String> messages = new LinkedList<String>();
+
+		@Override
+		public void run() {
+			while (true) {
+				synchronized (messages) {
+					if (messages.isEmpty()) {
+						Thread.yield();
+					} else {
+						System.out.println(messages.remove());
+					}
+				}
+			}
+		}
+
+		public void log(String message) {
+			synchronized (message) {
+				messages.add(message);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+
 		ExecutorService pool = Executors.newFixedThreadPool(10);
+		Logger logger = new Logger();
+		pool.execute(logger);
 
 		for (int i = 0; i < 20; i++) {
-			pool.execute(new Runnable() {
-
-				@Override
-				public void run() {
-					int n = RANDOM.nextInt(40);
-					System.out.format("fib(%d) = %s\n", n, fib(n));
-				}
-			});
+			pool.execute(new FibonacciTask(logger));
 		}
 
 		pool.shutdown();
 		pool.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
 		System.out.println("done");
-
 	}
 
 }
