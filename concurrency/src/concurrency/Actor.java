e24d0e8ec40782b04ecd462989b7b85d1ff55ed1
fabien <fabien.kunis@gmail.com>
2015-05-28 08:24:30 +0300

    Add Actor model

---
+++ concurrency/src/concurrency/Actor.java
@@ -0,0 +1,33 @@
+package concurrency;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public abstract class Actor implements Runnable {
+	private final BlockingQueue<Object> messages = new LinkedBlockingQueue<Object>();
+	private boolean shutdown = false;
+
+	public final void tell(Object message) {
+		try {
+			messages.put(message);
+		} catch (InterruptedException e) {
+		}
+	}
+
+	protected final void shutdown() {
+		shutdown = true;
+	}
+
+	protected abstract void receive(Object message);
+
+	@Override
+	public final void run() {
+		while (!shutdown) {
+			try {
+				receive(messages.take());
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+}
--- concurrency/src/concurrency/Fibonacci.java
+++ concurrency/src/concurrency/Fibonacci.java
@@ -1,66 +1,79 @@
 package concurrency;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class Fibonacci {
 
-	private static final Random RANDOM = new Random();
-
-	public static BigInteger fib(int n) {
-		return n < 2 ? BigInteger.ONE : fib(n - 1).add(fib(n - 2));
+	public static enum Message {
+		FIB, SHUTDOWN
 	}
 
-	public static class FibonacciTask implements Runnable {
+	public static class FibonacciTask extends Actor {
 
+		private static final Random RANDOM = new Random();
 		private final Logger logger;
+
+		private static BigInteger fib(int n) {
+			return n < 2 ? BigInteger.ONE : fib(n - 1).add(fib(n - 2));
+		}
 
 		public FibonacciTask(Logger logger) {
 			this.logger = logger;
+			this.tell(Message.FIB);
 		}
 
-		@Override
-		public void run() {
-			int n = RANDOM.nextInt(30);
-			logger.log(String.format("fib(%d) = %s", n, fib(n)));
+		protected void receive(Object message) {
+			if (message == Message.FIB) {
+				int n = RANDOM.nextInt(30);
+				logger.tell(String.format("fib(%d) = %s", n, fib(n)));
+				this.tell(Message.FIB);
+			} else if (message == Message.SHUTDOWN) {
+				this.shutdown();
+			}
 		}
 	}
 
-	public static class Logger implements Runnable {
-		private final BlockingQueue<String> messages = new LinkedBlockingQueue<String>();
-
-		@Override
-		public void run() {
-			while (true) {
-				try {
-					System.out.println(messages.take());
-				} catch (InterruptedException e) {
-				}
+	public static class Logger extends Actor {
+		protected void receive(Object message) {
+			if (message == Message.SHUTDOWN) {
+				this.shutdown();
+			} else {
+				System.out.println(message);
 			}
 		}
-
-		public void log(String message) {
-			try {
-				messages.put(message);
-			} catch (InterruptedException e) {
-			}
-		}
-
 	}
 
 	public static void main(String[] args) throws Exception {
 
+		// create a thread pool and an empty list of actors
 		ExecutorService pool = Executors.newFixedThreadPool(10);
+		List<Actor> actors = new ArrayList<Actor>();
+
+		// create a logger, execute it and add it to the list
 		Logger logger = new Logger();
 		pool.execute(logger);
+		actors.add(logger);
 
-		while (true) {
-			pool.execute(new FibonacciTask(logger));
+		// create fibonacci tasks, execute them and add them to the list
+		for (int i = 1; i < 10; i++) {
+			FibonacciTask task = new FibonacciTask(logger);
+			pool.execute(task);
+			actors.add(task);
 		}
+
+		// wait for user to hit enter, and tell all actors to shut down
+		System.in.read();
+		for (Actor actor : actors) {
+			actor.tell(Message.SHUTDOWN);
+		}
+
+		// shut down the thread pool
+		pool.shutdown();
 	}
 
 }
